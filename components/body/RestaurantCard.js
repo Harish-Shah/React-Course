753e4fecc2649805f487952927af04ecc2cdae94
Harish Shah <harishah9867@gmail.com>
2024-07-27 09:16:35 +0530

    added restaurant cards by mapping the Data from the swiggy api list

--- components/body/RestaurantCard.js
+++ components/body/RestaurantCard.js
@@ -1,14 +1,21 @@
-const RestaurantCard = (props) => {
-  return (
+const RestaurantCard = ({
+  name,
+  cuisines,
+  cloudinaryImageId,
+  lastMileTravelString,
+}) => {
+
+   return (
     <div className="res-card">
       <img
-        className="res-dish"
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_292,h_300/TopPicks2024/3387952A.png"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
       />
-      <h3>{props.resName}</h3>
-      <h4>{props.cuisine}</h4>
-      <h4>4.4 stars</h4>
-      <h4>35 mins</h4>
+      <h2>{name}</h2>
+      <h3>{cuisines.join(", ")}</h3>
+      <h4>{lastMileTravelString} minutes</h4>
     </div>
   );
 };
